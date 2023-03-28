349469b5b40ca8faba5f5a919afe4e937e91d4b9
Herschenglime <herschenglime@gmail.com>
2023-03-28 01:05:50 -0400

    added placeholder journal entry

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
 
 	kit: {
 		adapter: adapter()
-	}
+	},
+
+	extensions: ['.svelte', '.md'], //what files to treat as components
+
+	preprocess: [
+		sveltePreprocess(),
+		mdsvex({
+			extensions: ['.md'] //let mdsvex process md files
+		})
+	]
 };
 
 export default config;
