1b73128aa3c81228c02e5981d03e5e039871cb66
Herschenglime <herschenglime@gmail.com>
2023-07-01 20:05:53 -0700

    silly page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
  import ThemeSwitch from '$lib/components/ThemeSwitch.svelte'
  import Navbar from '$lib/components/Navbar.svelte'
  // import '$lib/styles/placeholder.scss' - if I wanted to import, although inline seems fine
+ import { fade } from 'svelte/transition'
+
+ export let data
 </script>
 
 <svelte:head>
@@ -12,7 +15,11 @@
 
 <Navbar/>
 
-<slot />
+{#key data.currentRoute}
+  <main in:fade={{ duration: 150, delay: 150 }} out:fade={{ durtion: 150 }}>
+    <slot/>
+  </main>
+{/key}
 
 <style>
  :root {
