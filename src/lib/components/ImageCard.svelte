6ca4ba76f1a8d0b6acf7dea44b7477f0b115ba11
Herschenglime <herschenglime@gmail.com>
2024-05-01 16:08:31 -0400

    add ImageCard component

---
+++ src/lib/components/ImageCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+ export let img:string
+ export let title:string
+ export let buttonLabel:string = "visit"
+</script>
+
+<div class="card lg:card-side bg-base-100 shadow-xl">
+  <figure>
+    <img src={img} alt="Album" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">{title}</h2>
+    <p>
+      <slot />
+    </p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">{buttonLabel}</button>
+    </div>
+  </div>
+</div>
+
