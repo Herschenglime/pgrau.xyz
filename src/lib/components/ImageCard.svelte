600f821c0a224ec3e2d9c5604eed24bfd265856d
Herschenglime <herschenglime@gmail.com>
2024-05-01 17:15:29 -0400

    Finalize ImageCard and add link to ufli site

--- src/lib/components/ImageCard.svelte
+++ src/lib/components/ImageCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  export let link: string;
   export let img: string;
   export let alt: string;
   export let title: string;
@@ -9,16 +10,16 @@
 <div class="not-prose">
   <div class="flex-none m-none card card-compact bg-base-200 shadow-xl">
     <figure>
-      <img src={img} {alt} />
+      <!-- extra things after href make it open in new tab -->
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img src={img} {alt} />
+      </a>
     </figure>
     <div class="card-body">
       <h2 class="card-title">{title}</h2>
       <p>
         <slot />
       </p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">{buttonLabel}</button>
-      </div>
     </div>
   </div>
 </div>
--- src/routes/e-portfolio/research-experience/content.md
+++ src/routes/e-portfolio/research-experience/content.md
@@ -10,9 +10,11 @@
 
 Working under the Digital Worlds Institute and in partnership with the University of Florida Literacy Institute (UFLI), my team created a number of web-based literacy games for use with the UFLI Foundations Curriculum for the use of K-2 Students. The working site can be seen here:
 
-<!-- https://research.dwi.ufl.edu/op.n/file/pd7py49630t41lba/ -->
 <ImageCard
     title="UFLI Games"
     alt="UFLI Games screenshot"
     img='$lib/content/e-portfolio/research-experience/ufli-games.png'
-/>
+    link='https://research.dwi.ufl.edu/op.n/file/pd7py49630t41lba/'
+>
+Click the image to be taken to the site.
+</ImageCard>
