02924c0e40558d8410fe3d15891bd497d5c615bb
Herschenglime <herschenglime@gmail.com>
2024-05-02 20:25:17 -0400

    swap old theming setup to use DaisyUI instead

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,11 @@ const config = {
 	plugins: [
 		require('@tailwindcss/typography'),
 		require('daisyui'),
-	]
+	],
+
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
 };
 
 module.exports = config;
